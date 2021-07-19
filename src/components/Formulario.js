225a205d7356faf6dd90fb1b98633549b06dc2cb
napoleon geronimo <napoleongeronimo06@gmail.com>
2021-07-18 21:02:11 -0300

    useState y envio de formulari o

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,16 +1,48 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Formulario = () => {
+    //crear state de cliente
+    const [turno, actualizarTurno] = useState({
+        producto: '',
+        cliente: '',
+        fecha: '',
+        hora: '',
+        descripcion: ''
+    });
+
+    //funcion que se ejecuta cuando el usario escribe en un imput
+    const actualizarState = e => {
+        actualizarTurno({
+            ...turno,
+            [e.target.name] : e.target.value
+        })
+        
+    }
+    //extraer los valores
+    const {producto, cliente, fecha, hora, descripcion} = turno;
+    //cuando el usuario precione se agrega el turno
+    const submitTurno = e => {
+        e.preventDefault();
+        //validar
+        //asignar un id
+        //crear turno
+        //reiniciar el form
+    }
+
     return ( 
         <Fragment>
             <h2>Crear Turno </h2>
-            <form>
+            <form
+                onSubmit={submitTurno}
+            >
                 <label>Nombre del Producto</label>
                 <input
                     type="text"
-                    name="cliente"
+                    name="producto"
                     className="u-full-width"
                     placeholder="Nombre Producto"
+                    onChange={actualizarState}
+                    value={producto}
                 />
                 <label>Nombre del Cliente</label>
                 <input
@@ -18,6 +50,8 @@ const Formulario = () => {
                     name="cliente"
                     className="u-full-width"
                     placeholder="Nombre Cliente"
+                    onChange={actualizarState}
+                    value={cliente}
                 />
                 <label>Fecha</label>
                 <input
@@ -25,18 +59,23 @@ const Formulario = () => {
                     name="fecha"
                     className="u-full-width"
                     placeholder="Nombre Cliente"
+                    onChange={actualizarState}
+                    value={fecha}
                 />
                 <label>Hora de entrega</label>
                 <input
                     type="time"
-                    name="cliente"
+                    name="hora"
                     className="u-full-width"
-                    
+                    onChange={actualizarState}
+                    value={hora}
                 />
                 <label>Descripcion</label>
                 <textarea 
                     className="u-full-width"
                     name="Descripcion"
+                    onChange={actualizarState}
+                    value={descripcion}
                 >
                 </textarea>
 
